1045cb126b6d2a379e558f00f1d0d45c25034aed
tomeytran <tmtri310251@gmail.com>
2024-09-24 09:24:14 +0700

    foot to meter

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-let n = 0;
-let count = 0;
-let number = +prompt("Nhap so");
+function footToMeter() {
+	let toMeter = document.getElementById("foot").value;
 
-function isPrime(n) {
-	if (n < 2) return false;
+	document.getElementById(
+		"footToMeter"
+	).innerHTML = `${toMeter} feets = ${Math.round(toMeter * 0.305)} meters`;
 
-	if (n < 4) return true;
-
-	for (let i = 2; i < n; i++) {
-		if (n % i == 0) return false;
-	}
-	return true;
+	document.getElementById("meter").value = Math.round(toMeter * 0.305);
 }
 
-while (count < number) {
-	if (isPrime(n)) {
-		document.write(n + "<br>");
-		count++;
-	}
-	n++;
+function meterToFoot() {
+	let toFooter = document.getElementById("meter").value;
+	document.getElementById("foot").value = Math.round(toFooter * 3.279);
 }
