b89725ce995db8bf25e3e430ad7d383317a6c30c
tomeytran <tmtri310251@gmail.com>
2024-09-24 09:27:54 +0700

    update

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function footToMeter() {
 	let toMeter = document.getElementById("foot").value;
 
-	document.getElementById(
-		"footToMeter"
-	).innerHTML = `${toMeter} feets = ${Math.round(toMeter * 0.305)} meters`;
-
 	document.getElementById("meter").value = Math.round(toMeter * 0.305);
 }
 
